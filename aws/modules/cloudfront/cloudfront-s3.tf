2ae64c118ebe48eaf280332be765ad49b11b627a
Walter Cruz <walter.cruz@tunubi.com>
2024-03-13 14:06:00 +0100

    aws cloudfront fix

--- aws/modules/cloudfront/cloudfront-s3.tf
+++ aws/modules/cloudfront/cloudfront-s3.tf
@@ -1,36 +1,175 @@
-resource "aws_eks_cluster" "eks-cluster" {
-  name     = var.settings["cluster_name"]
-  role_arn = aws_iam_role.eks-cluster.arn
-  version  = "1.18"
+resource "aws_s3_bucket" "cloudfront-b" {
+  for_each = toset(var.bucketName) 
+  bucket = each.key
+  acl    = "public-read-write"
 
-  vpc_config {
-    endpoint_public_access  = false
-    endpoint_private_access = true
-    subnet_ids              = var.subnets #[for subnet in data.aws_subnet_ids.subnets.ids: subnet]
-    security_group_ids      = [aws_security_group.secgroup.id]
-
+  tags = {
+    Name = each.key
   }
+}
 
-  depends_on = [
-    aws_iam_role.eks-cluster,
+resource "aws_s3_bucket_policy" "cloudfront-b" {
+  for_each = aws_s3_bucket.cloudfront-b
+  bucket = each.key 
+  policy = <<EOF
+{
+"Version": "2008-10-17",
+"Statement": [
+    {
+        "Sid": "2",
+        "Effect": "Allow",
+        "Principal": {
+            "AWS": "${aws_cloudfront_origin_access_identity.s3_access.iam_arn}"
+        },
+        "Action": "s3:*",
+        "Resource": "${each.value.arn}/*"
+    }
   ]
-  provisioner "local-exec" {
-    command = "aws eks --region ${ var.settings.region } update-kubeconfig --name ${ var.settings.cluster_name } --profile ${ var.profile }"
+}
+EOF 
+depends_on = [
+    aws_cloudfront_origin_access_identity.s3_access,
+    aws_s3_bucket.cloudfront-b,
+  ]
+}
+
+resource "aws_cloudfront_origin_access_identity" "s3_access" {
+  comment = "s3-cloudfront-b"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  dynamic "origin" {
+        for_each = [for i in var.dynamic_s3_origin_config : {
+          name          = i.domain_name
+          id            = i.origin_id
+          identity      = lookup(i, "origin_access_identity", null)
+          path          = lookup(i, "origin_path", "")
+        }]
+
+        content {
+          domain_name = origin.value.name
+          origin_id   = origin.value.id
+          origin_path = origin.value.path
+          s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.s3_access.cloudfront_access_identity_path
+          }
+        }
+      }
+      
+  dynamic "origin" {
+      for_each = [for i in var.dynamic_custom_origin_config : {
+        name                     = i.domain_name
+        id                       = i.origin_id
+        path                     = lookup(i, "origin_path", "")
+      }]
+      content {
+        domain_name = origin.value.name
+        origin_id   = origin.value.id
+        origin_path = origin.value.path
+        custom_origin_config {
+          http_port                = "80"
+          https_port               = "443"
+          origin_keepalive_timeout = "5"
+          origin_read_timeout      = "30"
+          origin_protocol_policy   = "https-only"
+          origin_ssl_protocols     = ["TLSv1.2", "TLSv1.1"]
+        }
+      }
+    }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = var.cfDescription
+  default_root_object = var.defaultRoot
+
+  aliases = ["${var.webname}${var.dot}${var.zonename}"]
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucketNameDefault
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
-  # provisioner "local-exec" {
-  #   command = "helm install spot-controller spotinst/spotinst-kubernetes-cluster-controller --set spotinst.token=${ var.spotToken } --set spotinst.account=${ var.settings.spotAccount } --set spotinst.clusterIdentifier=${ var.settings["cluster_name"] }"
-  # }
+
+  # Cache behavior with precedence 0
+  dynamic "ordered_cache_behavior" {
+    for_each = var.behavior[*]
+    iterator = w
+    content {
+      path_pattern     = w.value.folder
+      allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+      cached_methods   = ["GET", "HEAD", "OPTIONS"]
+      target_origin_id = w.value.destination
+
+      forwarded_values {
+        query_string = false
+        headers      = ["Origin"]
+
+        cookies {
+          forward = "none"
+        }
+      }
+
+      min_ttl                = 0
+      default_ttl            = 86400
+      max_ttl                = 31536000
+      compress               = true
+      viewer_protocol_policy = "redirect-to-https"
+    }
+  }
+  
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn = data.aws_acm_certificate.zoneCert.arn
+    ssl_support_method = "sni-only"
+  }
+depends_on = [
+    aws_cloudfront_origin_access_identity.s3_access,
+    aws_s3_bucket.cloudfront-b,
+    data.aws_acm_certificate.zoneCert
+  ]
+}
+data "aws_route53_zone" "zone" {
+  name         = var.zonename
+
 }
 
-data "aws_eks_cluster" "eks-cluster" {
-    name     = var.settings["cluster_name"]
-    depends_on = [aws_eks_cluster.eks-cluster]
+data "aws_acm_certificate" "zoneCert" {
+  provider = aws.certificates
+  domain = "*.${var.zonename}"
 }
 
-data "aws_eks_cluster_auth" "eks-cluster" {
-  name     = var.settings["cluster_name"]
-}
-
-output "clusterArn" {
-  value = aws_eks_cluster.eks-cluster.arn
-}
+resource "aws_route53_record" "cloudfront-b" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.webname
+  type    = "CNAME"
+  ttl     = "600"
+  records = [aws_cloudfront_distribution.s3_distribution.domain_name]
+} 
